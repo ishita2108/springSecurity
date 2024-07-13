59afab3e91a839963a5e339af3185f22c69f9ccb
ishita2108 <iwinner21@gmail.com>
2024-07-13 16:52:13 +0530

    JdbcUserDetailsManager

--- src/main/java/com/spring_security/securitydemo/SecurityConfig.java
+++ src/main/java/com/spring_security/securitydemo/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.spring_security.securitydemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,6 +24,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -39,6 +46,11 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService(){
         UserDetails user1 = User.withUsername("user1").password("{noop}password1").roles("USER").build();
         UserDetails admin = User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user1,admin);
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user1);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+        //return new InMemoryUserDetailsManager(user1,admin);
     }
 }
