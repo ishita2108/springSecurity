1b74245503f945a22da0d6010939d54fec30ea2e
ishita2108 <iwinner21@gmail.com>
2024-07-13 16:39:39 +0530

    Upto InMemoryUserDetail

---
+++ src/main/java/com/spring_security/securitydemo/GreetingsController.java
@@ -0,0 +1,27 @@
+package com.spring_security.securitydemo;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello!!";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint(){
+        return "Hello User!!";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint(){
+        return "Hello Admin!!";
+    }
+}
